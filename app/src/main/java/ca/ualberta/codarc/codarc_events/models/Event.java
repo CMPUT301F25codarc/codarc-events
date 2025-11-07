8453a6edbf28c4c04a82b78e909ae1ad49c51299
Subhan <130109821+Subhan-Noor@users.noreply.github.com>
2025-11-07 15:21:11 -0700

    add javadoc

--- app/src/main/java/ca/ualberta/codarc/codarc_events/models/Event.java
+++ app/src/main/java/ca/ualberta/codarc/codarc_events/models/Event.java
@@ -22,6 +22,20 @@ public class Event implements Serializable {
 
     public Event() { }
 
+
+    /**
+     * Creates an Event with the given details.
+     *
+     * @param id event ID
+     * @param name event name
+     * @param description event description
+     * @param eventDateTime date and time of the event
+     * @param registrationOpen when registration opens
+     * @param registrationClose when registration closes
+     * @param open true if the event is currently open
+     * @param organizerId ID of the event organizer
+     * @param qrCode QR code string for the event
+     */
     public Event(String id, String name, String description,
                  String eventDateTime, String registrationOpen,
                  String registrationClose, boolean open, String organizerId, String qrCode) {
@@ -38,30 +52,68 @@ public class Event implements Serializable {
 
 
     // Getters
+    /** @return the event ID */
     public String getId() { return id; }
+
+    /** @return the event name */
     public String getName() { return name; }
+
+    /** @return the event description */
     public String getDescription() { return description; }
+
+    /** @return the date and time of the event */
     public String getEventDateTime() { return eventDateTime; }
+
+    /** @return when registration opens */
     public String getRegistrationOpen() { return registrationOpen; }
+
+    /** @return when registration closes */
     public String getRegistrationClose() { return registrationClose; }
+
+    /** @return true if the event is open */
     public boolean isOpen() { return open; }
+
+    /** @return the organizer's ID */
     public String getOrganizerId() { return organizerId; }
 
+    /** @return the event QR code */
     public String getQrCode() { return qrCode; }
+
+    /** @return the maximum capacity */
     public Integer getMaxCapacity() { return maxCapacity; }
+
+    /** @return the event location */
     public String getLocation() { return location; }
 
     // Setters
+
+    /** @param id sets the event ID */
     public void setId(String id) { this.id = id; }
+    /** @param name sets the event name */
     public void setName(String name) { this.name = name; }
+    /** @param description sets the event description */
     public void setDescription(String description) { this.description = description; }
+
+    /** @param eventDateTime sets the date and time of the event */
     public void setEventDateTime(String eventDateTime) { this.eventDateTime = eventDateTime; }
+
+    /** @param registrationOpen sets when registration opens */
     public void setRegistrationOpen(String registrationOpen) { this.registrationOpen = registrationOpen; }
+
+    /** @param registrationClose sets when registration closes */
     public void setRegistrationClose(String registrationClose) { this.registrationClose = registrationClose; }
+
+    /** @param open sets whether the event is open */
     public void setOpen(boolean open) { this.open = open; }
+    /** @param organizerId sets the organizer ID */
     public void setOrganizerId(String organizerId) { this.organizerId = organizerId; }
+    /** @param qrCode sets the event QR code */
     public void setQrCode(String qrCode) { this.qrCode = qrCode; }
+
+    /** @param maxCapacity sets the maximum capacity */
     public void setMaxCapacity(Integer maxCapacity) { this.maxCapacity = maxCapacity; }
+
+    /** @param location sets the event location */
     public void setLocation(String location) { this.location = location; }
 }
 
